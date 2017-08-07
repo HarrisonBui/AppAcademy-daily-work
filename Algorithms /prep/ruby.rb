576dd413e249373717f598228858181278e99816
Harrison Bui <hsbui@hotmail.com>
2017-08-06 20:14:12 -0700

    lca

---
+++ Algorithms /prep/ruby.rb
@@ -0,0 +1,35 @@
+# Given two nodes in a binary tree, design an algorithm
+# that computes their Least Common Ancestor (LCA).
+# Assume that each node has a parent pointer.
+
+def lca(node_0, node_1)
+  depth_0 = get_depth(node_0)
+  depth_1 = get_depth(node_1)
+
+  if depth_1 > depth_0
+    node_0, node_1 = node_1, node_0
+  end
+
+  depth_diff = (depth_0 - depth_1).abs
+  while depth_diff
+    node_0 = node_0.pare
+    nt
+    depth_diff -= 1
+  end
+
+  while node_0 != node_1
+    node_0 = node_0.parent
+    node_1 = node_1.parent
+  end
+
+  node_0
+end
+
+def get_depth(node)
+  depth = 0
+  while node
+    depth += 1
+    node = node.parent
+  end
+  depth
+end
