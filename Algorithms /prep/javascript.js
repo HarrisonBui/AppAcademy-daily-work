7776b6f3e7470025c7c1e42ee50b69f6ebd0b95f
Harrison Bui <hsbui@hotmail.com>
2017-08-06 20:14:29 -0700

    valid palindrome

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -3,3 +3,37 @@ for (var i = 1; i <= 100; i++) {
   var f = i % 3 == 0, b = i % 5 == 0;
   console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
 }
+
+
+//valid palindrome
+function isPalindrome(str) {
+  var i = 0,
+      j = str.length - 1,
+      str = str.toLowerCase();
+
+  if (str.length == 0){
+    return true;
+  }
+
+  while (i < j) {
+
+    if(/[^a-zA-Z0-9]/.test(str[i])) {
+      i++;
+      continue;
+    }
+
+    if(/[^a-zA-Z0-9]/.test(str[j])) {
+      j--;
+      continue;
+    }
+
+    if(str[i] != str[j]) {
+      return false;
+    }
+
+    i++;
+    j--;
+  }
+
+  return true;
+}
