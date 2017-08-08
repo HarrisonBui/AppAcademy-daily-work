92436ffc3c5b7159af2fdb6211654914c71934d5
Harrison Bui <hsbui@hotmail.com>
2017-08-07 22:44:41 -0700

    valid_ip

--- Algorithms /prep/ruby.rb
+++ Algorithms /prep/ruby.rb
@@ -33,3 +33,9 @@ def get_depth(node)
   end
   depth
 end
+
+def valid_ip?(str)
+  return false unless str =~ /^\d+(\.\d+){3}$/
+  nums = str.split(".").map(&:to_i)
+  nums.all? {|num| num >= 0 && num <= 255}
+end
