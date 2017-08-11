d372cc08ba8828d520c427b3ccb44dde3cb8703f
Harrison Bui <hsbui@hotmail.com>
2017-08-10 17:10:48 -0700

    delete node LinkedList

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -138,3 +138,52 @@ function eliminateDuplicates(arr) {
   }
   return out;
 }
+
+
+//delete a node from a linked list
+'use strict';
+
+function LinkedList () {
+    this.head = null;
+}
+
+LinkedList.prototype.deleteNode = function (val) {
+
+    if (this.head.data === val) {
+        this.head = this.head.next;
+    } else {
+        var previousNode = this.head;
+        var currentNode = previousNode.next;
+        while (currentNode) {
+            if (currentNode.data === val) {
+                previousNode.next = currentNode.next;
+                currentNode = currentNode.next;
+                break;
+            } else {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+            }
+        }
+    }
+};
+
+// Create an instance of a LinkedList class
+var L1 = new LinkedList();
+
+// Create a linked list with six elements
+L1.insertNodeAtTail(5);
+L1.insertNodeAtTail(6);
+L1.insertNodeAtTail(7);
+L1.insertNodeAtTail(8);
+L1.insertNodeAtTail(9);
+L1.insertNodeAtTail(10);
+console.log(L1);
+
+// Delete a head and a tail node
+L1.deleteNode(5);
+L1.deleteNode(10);
+console.log(L1);
+
+// Delete  an intermediate node
+L1.deleteNode(7);
+console.log(L1);
