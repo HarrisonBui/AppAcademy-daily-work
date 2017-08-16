e2049e82f4baab93276a1ead34d74037fd8c6b31
Harrison Bui <hsbui@hotmail.com>
2017-08-15 22:43:24 -0500

    insertionSortList

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -219,3 +219,22 @@ var missingNumber = function(nums) {
     if (!hash[i])
       return i;
 };
+
+//intergetSortList
+var insertionSortList = function(head) {
+  var ans = [];
+  while (head) {
+    ans.push(new ListNode(head.val));
+    head = head.next;
+  }
+
+  ans.sort(function(a, b) {
+    return a.val - b.val;
+  });
+
+  if (!ans.length) return null;
+  for (var i = 0, len = ans.length; i < len - 1; i++)
+    ans[i].next = ans[i + 1];
+
+  return ans[0];
+};
