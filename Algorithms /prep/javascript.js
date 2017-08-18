c2f392b2742822d83b7130bd76b6104803d72ceb
Harrison Bui <hsbui@hotmail.com>
2017-08-18 18:05:12 -0500

    firstMissingPositive

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -238,3 +238,19 @@ var insertionSortList = function(head) {
 
   return ans[0];
 };
+
+//first missing possitive
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+  var hash = [];
+  for(var i = 0, len = nums.length; i < len; i++)
+    if (nums[i] <= 0) continue;
+    else hash[nums[i]] = true;
+
+  for(var i = 1; ; i++)
+    if (!hash[i])
+      return i;
+};
