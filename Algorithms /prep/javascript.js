f064d63e1d769d2245b4afa5a39cb361fd825340
Harrison Bui <hsbui@hotmail.com>
2017-08-24 17:03:27 -0500

    gray code

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -501,3 +501,12 @@ var getRow = function(rowIndex) {
 
   return ans[rowIndex];
 };
+
+
+//Gray code
+var grayCode = function(n) {
+  var ans = [];
+  for (var i = 0; i < (1 << n); i++)
+    ans[i] = i ^ (i >> 1);
+  return ans;
+};
