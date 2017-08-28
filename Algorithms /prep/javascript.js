b1617c9e239f6f213397ea21ea5c15a779f69c65
Harrison Bui <hsbui@hotmail.com>
2017-08-27 23:19:49 -0500

    next permutation

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -584,3 +584,44 @@ var reverse = function(x) {
   else
     return ans;
 };
+
+
+//next Permutation
+var nextPermutation = function(nums) {
+  var len = nums.length;
+
+  var flag = false;
+
+  var pos, changePos;
+
+  for (var i = len - 2; i >= 0; i--) {
+    if (nums[i] < nums[i + 1]) {
+      flag = true;
+      pos = i;
+      break;
+    }
+  }
+
+  if (!flag) {
+    nums.sort(function(a, b) {
+      return a - b;
+    });
+  } else {
+    for (var i = len - 1; ; i--)
+      if (nums[i] > nums[pos]) {
+        var tmp = nums[i];
+        nums[i] = nums[pos];
+        nums[pos] = tmp;
+        break;
+      }
+
+    var tmp = nums.slice(pos + 1).sort(function(a, b) {
+      return a - b;
+    });
+
+    nums.length = pos + 1;
+
+    Array.prototype.push.apply(nums, tmp);
+  }
+
+};
