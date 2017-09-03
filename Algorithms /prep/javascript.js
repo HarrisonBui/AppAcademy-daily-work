44414a448b7c253740d6931749997ed687e66dd6
Harrison Bui <hsbui@hotmail.com>
2017-09-02 20:26:57 -0500

    license key formatting

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -763,4 +763,20 @@ var countPrimes = function(n) {
       ans ++;
 
   return ans;
+
+
+//License Key Formatting
+var format = function(chars, length, separator) {
+    if (chars.length <= length) {
+        return chars.join('');
+    }
+    return format(chars.slice(0, chars.length - length), length, separator) + separator + chars.slice(-length).join('');
+};
+var licenseKeyFormatting = function(S, K) {
+    var chars = S.split('').filter((char) => {
+        return char !== '-';
+    });
+
+    return format(chars, K, '-').toUpperCase();
+
 };
