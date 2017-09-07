12e1804b53394b9007de35652ccc056592663029
Harrison Bui <hsbui@hotmail.com>
2017-09-06 21:59:25 -0500

    third maximum difference

--- Algorithms /prep/javaQ.java
+++ Algorithms /prep/javaQ.java
@@ -1,3 +1,4 @@
+//Find Min Difference
 public class Solution {
     public int findMinDifference(List<String> timePoints) {
         boolean[] mark = new boolean[24 * 60];
@@ -25,5 +26,31 @@ public class Solution {
         min = Math.min(min, (24 * 60 - last + first));
 
         return min;
+    }
+}
+
+
+//Third Maximum Number
+public class Solution {
+    public int thirdMax(int[] nums) {
+        int index = 0;
+        long[] result = {Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE};
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > result[0]) {
+                result[2] = result[1];
+                result[1] = result[0];
+                result[0] = nums[i];
+                index++;
+            } else if (nums[i] > result[1] && nums[i] < result[0]) {
+                result[2] = result[1];
+                result[1] = nums[i];
+                index++;
+            } else if (nums[i] > result[2] && nums[i] < result[1]) {
+                result[2] = nums[i];
+                index++;
+            }
+        }
+        return index > 2 ? (int)result[2] : (int)result[0];
     }
 }
