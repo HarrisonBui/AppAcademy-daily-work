14df1bbf3ee51f6ee2f6dffde7243297bc964a9a
Harrison Bui <hsbui@hotmail.com>
2017-09-09 19:20:36 -0500

    read binary watch

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -929,3 +929,34 @@ var findMinArrowShots = function(points) {
 
   return ans;
 };
+
+
+//Read Binary Watch
+var readBinaryWatch = function(num) {
+  let hours = [1, 2, 4, 8];
+  let minutes = [1, 2, 4, 8, 16, 32];
+  let ans = [];
+
+  dfs(num, 0, 0, 0, 0);
+
+  function dfs(left, a, b, hoursTotal, minutesTotal) {
+    if (hoursTotal >= 12 || minutesTotal >= 60)
+      return;
+
+    if (left === 0) {
+      let str = hoursTotal + ":";
+      str += minutesTotal < 10 ? '0' + minutesTotal : minutesTotal;
+      ans.push(str);
+      return;
+    }
+
+    for (let i = a; i < 4; i++) {
+      dfs(left - 1, i + 1, b, hoursTotal + hours[i], minutesTotal);
+    }
+
+    for (let i = b; i < 6; i++) {
+      dfs(left - 1, a, i + 1, hoursTotal, minutesTotal + minutes[i]);
+    }
+  }
+
+  return Array.from(new Set(ans));
