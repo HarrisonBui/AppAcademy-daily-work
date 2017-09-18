5ea25cf26d9a2e6a027493976eee8fa118a7ca6d
Harrison Bui <hsbui@hotmail.com>
2017-09-17 23:49:46 -0500

    ugly number

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1062,3 +1062,17 @@ var rotate = function(nums, k) {
 
   Array.prototype.unshift.apply(nums, tmp);
 };
+
+//Ugly Number
+var isUgly = function(num) {
+  if (num === 0)
+    return false;
+
+  var a = [2, 3, 5];
+  for (var i = 0; i < 3; i++) {
+    while (num % a[i] === 0)
+      num /= a[i];
+  }
+
+  return num === 1 ? true : false;
+};
