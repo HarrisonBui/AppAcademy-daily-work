53ca1f292b5b2db74a184a9e2aa2550fee31300f
Harrison Bui <hsbui@hotmail.com>
2017-09-19 15:13:08 -0500

    word pattern

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1091,3 +1091,35 @@ var nthUglyNumber = function(n) {
 
   return dp[n - 1];
 };
+
+//Word Pattern
+var wordPattern = function(pattern, str) {
+  var arr = str.split(' ');
+
+  if (pattern.length !== arr.length)
+    return false;
+
+  var a2b = {}
+    , b2a = {};
+
+  for (var i = 0, len = pattern.length; i < len; i++) {
+    var a = pattern[i]
+      , b = arr[i];
+
+    if (!a2b[a]) {
+      a2b[a] = b;
+    } else {
+      if (a2b[a] !== b)
+        return false;
+    }
+
+    if (!b2a[b]) {
+      b2a[b] = a;
+    } else {
+      if (baa[b] !== a)
+        return false;
+    }
+  }
+
+  return true;
+};
