736b856704bb26a688921661d9b8011936d7e334
Harrison Bui <hsbui@hotmail.com>
2017-09-23 21:06:02 -0700

    valid perfect square

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1155,3 +1155,24 @@ var integerBreak = function(n) {
 
   return dp[n];
 };
+
+
+//Valid Perfect Square
+var isPerfectSquare = function(num) {
+  var a = 0
+    , b = num;
+
+  while (a <= b) {
+    var mid = (a + b) >> 1
+      , ans = mid * mid;
+
+    if (ans > num)
+      b = mid - 1;
+    else if (ans < num)
+      a = mid + 1;
+    else
+      return true;
+  }
+
+  return false;
+};
