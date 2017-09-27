aaf9990128298a686dd0e624a6747efdedfb8ce8
Harrison Bui <hsbui@hotmail.com>
2017-09-27 14:37:31 -0700

    next greater element

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1269,3 +1269,28 @@ var makesquare = function(nums) {
 
   return isOk;
 };
+
+
+//Next Greater Element
+var nextGreaterElement = function(findNums, nums) {
+  let ans = [];
+  let len = nums.length;
+
+  findNums.forEach((item) => {
+    let pos = nums.indexOf(item);
+    let hasNextGreaterElement = false;
+
+    for (let i = pos + 1; i < len; i++) {
+      if (nums[i] > item) {
+        ans.push(nums[i]);
+        hasNextGreaterElement = true;
+        break;
+      }
+    }
+
+    if (!hasNextGreaterElement)
+      ans.push(-1);
+  });
+
+  return ans;
+};
