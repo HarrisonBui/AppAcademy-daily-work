a47aab07f8e43b313c73c872598782c769c29b79
Harrison Bui <hsbui@hotmail.com>
2017-10-01 16:07:27 -0700

    find right interval

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1334,3 +1334,39 @@ var nextGreaterElements = function(nums) {
 
   return ans;
 };
+
+
+//Find Right Interval
+var findRightInterval = function(intervals) {
+  intervals.forEach(function(item, index) {
+    item.index = index;
+  });
+
+  intervals.sort(function(a, b) {
+    return a.start - b.start;
+  });
+
+  let len = intervals.length;
+  for (let i = 0; i < len; i++) {
+    let f = true;
+    for (let j = i + 1; j < len; j++) {
+      if (intervals[j].start >= intervals[i].end) {
+        intervals[i].right = intervals[j].index;
+        f = false;
+        break;
+      }
+    }
+    f && (intervals[i].right = -1);
+  }
+
+  intervals.sort(function(a, b) {
+    return a.index - b.index;
+  });
+
+  let ans = [];
+  intervals.forEach(function(item) {
+    ans.push(item.right);
+  });
+
+  return ans;
+};
