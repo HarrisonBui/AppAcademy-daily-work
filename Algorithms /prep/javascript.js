cc512be7dbd19e135d1dcd096c8a567f1874d15f
Harrison Bui <hsbui@hotmail.com>
2017-10-05 14:50:54 -0700

    find bottom left value

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1450,3 +1450,20 @@ var addTwoNumbers = function(l1, l2) {
 
   return ret[0];
 };
+
+
+//Find Bottom Left Tree value
+var findBottomLeftValue = function(root) {
+  let res = [];
+
+  let dfs = (node, step) => {
+    if (res[step] === undefined)
+      res[step] = node.val;
+
+    node.left && dfs(node.left, step + 1);
+    node.right && dfs(node.right, step + 1);
+  };
+
+  dfs(root, 0);
+  return res.pop();
+};
