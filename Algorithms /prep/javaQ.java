05d91fe06c8012cfb8a96530309eda4f63fbfc29
Harrison Bui <hsbui@hotmail.com>
2017-10-06 16:19:45 -0700

    Number of islands

--- Algorithms /prep/javaQ.java
+++ Algorithms /prep/javaQ.java
@@ -30,7 +30,7 @@ public class Solution {
 }
 
 
-//Third Maximum Number 
+//Third Maximum Number
 public class Solution {
     public int thirdMax(int[] nums) {
         int index = 0;
@@ -53,4 +53,36 @@ public class Solution {
         }
         return index > 2 ? (int)result[2] : (int)result[0];
     }
+}
+
+
+//Number of Islands
+public class Solution {
+
+private int n;
+private int m;
+
+public int numIslands(char[][] grid) {
+    int count = 0;
+    n = grid.length;
+    if (n == 0) return 0;
+    m = grid[0].length;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == '1') {
+                DFSMarking(grid, i, j);
+                ++count;
+            }
+    }
+    return count;
+}
+
+private void DFSMarking(char[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+    grid[i][j] = '0';
+    DFSMarking(grid, i + 1, j);
+    DFSMarking(grid, i - 1, j);
+    DFSMarking(grid, i, j + 1);
+    DFSMarking(grid, i, j - 1);
+}
 }
