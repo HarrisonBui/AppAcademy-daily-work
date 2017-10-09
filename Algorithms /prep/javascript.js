6fab09dc986e5b4013ec3441294605ab574608c8
Harrison Bui <hsbui@hotmail.com>
2017-10-08 22:03:35 -0700

    First bad version

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1504,3 +1504,29 @@ var DFSMarking = function(grid, i , j) {
   DFSMarking(grid, i, j + 1);
   DFSMarking(grid, i, j - 1);
 }
+
+
+//First Bad Version
+
+
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        var start = 1;
+        var end = n;
+
+        while (start < end) {
+            var mid = start + (end - start) / 2;
+            if(!isBadVersion(mid)) {
+                end = mid;
+            }else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    };
+};
