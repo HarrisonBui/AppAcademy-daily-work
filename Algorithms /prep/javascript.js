996e2de2201443d5015b2ebcffcb0ec9f8b454f2
Harrison Bui <hsbui@hotmail.com>
2017-10-10 17:24:38 -0700

    Rotate image

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1585,3 +1585,24 @@ function firstNotRepeatingCharacter(string) {
 
   return "_";
 }
+
+
+//Rotate Image
+function rotateImage(a) {
+    // Transpose
+    for(var i = 0;i<a.length;i++){
+        for(var j = 0;j<i;j++){
+            // Switch a[i][j] and a[j][i]
+            // With XOR swap
+            a[i][j] ^= a[j][i]
+            a[j][i] ^= a[i][j]
+            a[i][j] ^= a[j][i]
+         }
+    }
+    // Reverse columns
+    for(var i in a){
+        a[i] = a[i].reverse()
+    }
+
+    return a
+}
