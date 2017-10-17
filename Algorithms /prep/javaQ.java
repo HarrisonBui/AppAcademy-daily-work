8ff63afa51f6968e35c951ccda34b753e3a8b970
Harrison Bui <hsbui@hotmail.com>
2017-10-16 22:04:39 -0700

    Jump game

--- Algorithms /prep/javaQ.java
+++ Algorithms /prep/javaQ.java
@@ -98,3 +98,14 @@ public int firstBadVersion(int n) {
   }
   return start;
 }
+
+
+//Jump Game
+public boolean canJump(int[] A) {
+    int max = 0;
+    for(int i=0;i<A.length;i++){
+        if(i>max) {return false;}
+        max = Math.max(A[i]+i,max);
+    }
+    return true;
+}
