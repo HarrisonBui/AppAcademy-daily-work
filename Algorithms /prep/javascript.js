db3c4c848fb288f22676d265b6869bb9da86d7de
Harrison Bui <hsbui@hotmail.com>
2017-11-27 23:44:35 -0800

    PairSum using a hash

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1842,3 +1842,19 @@ var findPeakElement = function(nums) {
       return i;
   }
 };
+
+
+let hashMapTwoSum = (array, sum) => {
+  let hashMap = {},
+      results = []
+
+  for(let i = 0; i < array.length; i++) {
+    if(hashMap[array[i]]) {
+      results.push([hashMap[array[i]], array[i]])
+    }else {
+      hashMap[sum - array[i]] = array[i]
+    }
+  }
+
+  return results
+}
