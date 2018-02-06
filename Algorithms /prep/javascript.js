4dce207154deca0c5bddadb7021a3c99f41c26fe
Harrison Bui <hsbui@hotmail.com>
2018-02-06 16:26:39 -0600

    create pascal triangle

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1967,3 +1967,22 @@ function letterCombinations(str) {
 
   return res;
 }
+
+//Create Pascal Triangle
+function createPascalTriangle (numRows) {
+  var pascalTriangle = [];
+
+  for (var i = 0; i < numRows; i++) {
+    pascalTriangle[i] = new Array(i+1);
+
+    for (var j = 0; j < i+1; j++) {
+      if (j === 0 || j === i) {
+        pascalTriangle[i][j] = 1;
+      } else {
+        pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+      }
+    }
+  }
+
+  return pascalTriangle;
+}
