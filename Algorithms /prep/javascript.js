2cfc3208402d9da13d11388f5086f49f7c4caf9e
Harrison Bui <hsbui@hotmail.com>
2018-02-05 22:08:58 -0600

    letter combination

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1931,3 +1931,39 @@ function exponent(base, power) {
 
     return num;
 }
+
+//Letter combination
+function letterCombinations(str) {
+   const map = new Map([
+  ['2', 'abc'],
+  ['3', 'def'],
+  ['4', 'ghi'],
+  ['5', 'jkl'],
+  ['6', 'mno'],
+  ['7', 'pqrs'],
+  ['8', 'tuv'],
+  ['9', 'wxyz']
+]);
+
+  if (str === '') return [];
+
+  let res = []
+
+  const bfs = (temp, left) => {
+    if (left.length === 0) {
+      res.push(temp)
+      return
+    }
+
+    let options = map.get(left[0])
+
+    for (let i = 0; i < options.length; i++) {
+      let item = options[i]
+      bfs(temp + item, left.slice(1, left.length))
+    }
+  }
+
+  bfs('', [...str])
+
+  return res;
+}
