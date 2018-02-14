06326189fabe0e2e5127233ad7d823d83b5a3c1a
Harrison Bui <hsbui@hotmail.com>
2018-02-13 22:21:36 -0600

    convert sorted array to binary tree

--- Algorithms /prep/javascript.js
+++ Algorithms /prep/javascript.js
@@ -1986,3 +1986,20 @@ function createPascalTriangle (numRows) {
 
   return pascalTriangle;
 }
+
+//Convert sorted array to binary tree
+let sortedArrayToBST = function(nums) {
+  return dfs(0, nums.length - 1);
+
+  function dfs(start, end) {
+    if (start > end)
+      return null;
+
+    var mid = (start + end) >> 1;
+    var node = new TreeNode(nums[mid]);
+    node.left = dfs(start, mid - 1);
+    node.right = dfs(mid + 1, end);
+
+    return node;
+  }
+};
